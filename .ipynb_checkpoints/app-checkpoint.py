8f9fb97a2fe984c931932858977f2f176881d521
Zannatun Nayeem <nayeem123able@gmail.com>
2020-10-23 18:21:59 +0600

    final build v1

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 from flask_bootstrap import Bootstrap
@@ -20,14 +19,24 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [float(x) for x in request.form.values()]
-    int_features = [np.array(int_features)]
-    final_features = scaler.transform(int_features)
+    pulse = float(request.form.get('pulse'))
+    blood = float(request.form.get('blood'))
+    temp = float(request.form.get('temp'))
+    age = float(request.form.get('age'))
+    height_feet = float(request.form.get('height_feet'))
+    height_inches = float(request.form.get('height_inches'))
+    weight = float(request.form.get('weight'))*2.20462
+    gender = float(request.form.get('gender'))
+    
+    height = (height_feet*12) + height_inches 
+    
+    all_features = [pulse,blood,temp,age,height,weight,gender]
+    all_features = [np.array(all_features)]
+    final_features = scaler.transform(all_features)
     prediction = model.predict(final_features)
+    #-------------------------------------------------
 
-    #output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text="Your Health Prediction is '{}'".format(prediction[0]))
+    return render_template('index.html', prediction_text="Your Health Prediction: {}".format(prediction[0]))
 
 
 if __name__ == "__main__":
